94d62d989574fe754325fad875398a2206084c5a
Alfredo Tito <tito.alfred@hotmail.com>
2023-06-17 23:00:37 -0300

    feat:react-hook-form-zod-lesson

--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -1,5 +1,7 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
     NEXT_PUBLIC_NEXT_ENV: 'development' | 'staging' | 'production'
+    SUPABASE_URL: string
+    SUPABASE_PASSWORD: string
   }
 }
